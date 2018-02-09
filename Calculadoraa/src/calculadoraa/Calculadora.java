b07cd42536292ee4aad12729d8b657181b36d63e
Gerónimo Puentes <>
2018-02-09 11:29:09 -0500

    Subir archivos creados por Netbeans.

--- Calculadoraa/src/calculadoraa/Calculadora.java
+++ Calculadoraa/src/calculadoraa/Calculadora.java
@@ -1,6 +1,26 @@
 package calculadoraa;
+
 public class Calculadora {
+
     int x;
     int y;
     int r;
+
+    void sumar() {
+        r = x + y;
+    }
+
+    void restar() {
+        if (x < y) {
+            int a;
+            a = x;
+            x = y;
+            y = a;
+        }
+        if (x < 0) {
+            x = -x;
+            y = -y;
+        }
+        r = x - y;
+    }
 }
