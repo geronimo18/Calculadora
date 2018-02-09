7882df1188d57870411c6fb296f882b301efca6b
Gerónimo Puentes <>
2018-02-09 11:45:54 -0500

    Codigo y funciones de la calculadora

---
+++ Calculadoraa/src/calculadoraa/Principal.java
@@ -0,0 +1,12 @@
+package calculadoraa;
+
+public class Principal {
+    
+    public static void main(String[] args){
+        //Crear un objeto de la clase Calculadora
+        
+        Calculadora micalculadora; //Definir el objeto
+        micalculadora = new Calculadora(); //Construir el objeto
+    }
+    
+}
