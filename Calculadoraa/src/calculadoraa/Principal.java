74dfd11f7bc16bdc925828977afa323338671c8b
Gerónimo Puentes <>
2018-02-12 17:58:18 -0500

    Taller de calculadora con herencia.

--- Calculadoraa/src/calculadoraa/Principal.java
+++ Calculadoraa/src/calculadoraa/Principal.java
@@ -1,12 +1,16 @@
 package calculadoraa;
 
+import java.util.Scanner;
+
 public class Principal {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         //Crear un objeto de la clase Calculadora
-        
+
         Calculadora micalculadora; //Definir el objeto
         micalculadora = new Calculadora(); //Construir el objeto
+        Scanner miEscaner;
+        miEscaner = new Scanner(System.in);
     }
     
 }
--- Calculadoraa/src/calculadoraa/Trigonometrica.java
+++ Calculadoraa/src/calculadoraa/Trigonometrica.java
@@ -2,8 +2,9 @@ package calculadoraa;
 
 import static java.lang.Math.*;
 
-public class Trigonometrica extends Calculadora{
-    double coseno(){
+public class Trigonometrica extends Calculadora {
+
+    double coseno() {
         return cos(x);
     }
     
